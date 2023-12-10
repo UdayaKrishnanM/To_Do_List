cf523a8dab37eafa93f76321509c195245ae253c
Udaya Krishnan.M <101326005+UdayaKrishnanM@users.noreply.github.com>
2023-12-10 16:21:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,28 +9,8 @@ import SearchItem from './SearchItem';
 
 function App() {
   
-  // const [items, setItems] = useState([
-  //   {
-  //       id: 1,
-  //       checked: true,
-  //       item: "One half pound bag of Coca Covered"
-  //   },
-  //   {
-  //       id: 2,
-  //       checked: false,
-  //       item: "Item 2"
-  //   },
-  //   {
-  //       id: 3,
-  //       checked: false,
-  //       item: "Item 3"
-  //   }
-  // ])
 
-
-  // below code is set because the added items are stored in localStorage while refreshing so that the objects are not removed
-
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')))
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist'))??[])
 
 
   // this will add new list 
@@ -54,10 +34,7 @@ function App() {
 
 
   const handleCheck = (id) => {
-    // below like of code is to convert a tick to untick when handleCheck is invoked or viceversa.
-    // here the above prop 'id' checked whether the invoked id is same or not. If it is same. The '!item.checked' converts to opposite of its value. 
-
-    // If true, it creates a new object with the spread (...) operator, copying all properties of the current item, but toggling the checked property to its opposite value using 
+    
     const listItems = items.map((item)=>item.id === id? { ...item, checked: !item.checked} : item)
     setAndSaveItems(listItems)
   }
@@ -68,12 +45,12 @@ function App() {
   }
 
   const handleSubmit = (e) => {
-    // below line does not allow page reload while submitting
+   
     e.preventDefault();
     if(!newItem) return;
-    // console.log(newItem)
+    
     addItem(newItem)
-    //addItem 
+   
     setNewItem('')
   }
 
