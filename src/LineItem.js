957ebf2580dcb6871359c0b8322b671f66d94241
Udaya Krishnan.M <101326005+UdayaKrishnanM@users.noreply.github.com>
2023-12-10 16:25:09 +0530

    Update LineItem.js

--- src/LineItem.js
+++ src/LineItem.js
@@ -6,10 +6,7 @@ const LineItem = ({ item, handleCheck, handleDelete }) => {
     <li className="item">
         <input
             type="checkbox"
-            // onChange is used in input tags of HTML. For button tags we use onClick for input tags use onChange
             onChange={()=> handleCheck(item.id)}
-            // below checked is used for ticking the box
-            // checked attribute is used to checkbox to get tick or not depends on the boolean value of item.che
             checked={item.checked}
         />
         <label
@@ -20,7 +17,6 @@ const LineItem = ({ item, handleCheck, handleDelete }) => {
             onClick={()=>handleDelete(item.id)}
             role="button" 
             tabIndex="0"
-            // Most JSX attribute are CamelCase but aria-lavel uses hypher
             aria-label={`Delete ${item.item}`}
             />
     </li>
