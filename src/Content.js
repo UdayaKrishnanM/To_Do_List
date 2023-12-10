bf4593c88dacb4ac9937a5c40e6f3c90701a5d27
Udaya Krishnan.M <101326005+UdayaKrishnanM@users.noreply.github.com>
2023-12-10 16:24:34 +0530

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,9 @@
-// import './index.css'
 import ItemList from './ItemList'
 
-// ({}) this is called prop drilling
 const Content = ({items, handleCheck, handleDelete}) => {
 
     return (
         <main>
-            {/*each react item in list needs a key attribute. This helpsreact which items have changed which have been added which items have been removed. Always list item needs a key items */}
-            {/* ternary operator is used to check whether the items length. If it is empty is shows "Your List is Empty" or it displays the "List" */}
             {items.length ? (
                 <ItemList
                     items = {items}
