1d1a93cfc3a47aea2ff474b21182588c70c6aacc
Udaya Krishnan.M <101326005+UdayaKrishnanM@users.noreply.github.com>
2023-12-10 16:24:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist'))??[])
 
 
-  // this will add new list 
   const [newItem, setNewItem] = useState('')
 
   const [search, setSearch] = useState('')
